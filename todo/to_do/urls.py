69403e98074d19cab443a31ea2871fdfacb683dc
Makeev Eduard <mckena@mail.ru>
2022-06-17 22:45:48 +0300

    lesson_1

--- todo/to_do/urls.py
+++ todo/to_do/urls.py
@@ -1,4 +1,4 @@
-"""todo URL Configuration
+"""to_do URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
